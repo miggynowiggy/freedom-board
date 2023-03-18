3986645bf53bdfcae0c66f731856bface6db1dd3
Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2023-03-18 16:34:00 +0800

    added ForgotPassword Page

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -1,5 +1,53 @@
+import { Link } from 'react-router-dom'
+import { Row, Col, Card, Form, Input, Button } from 'antd'
 export default function ForgotPasswordPage() {
   return (
-    <h1>forgot password page</h1>
+    <Row
+      style={{ height: '100vh', width: '100%' }}
+      align="middle"
+      justify="center"
+    >
+      <Col span={12}>
+        <Card
+          title="Login"
+          style={{ width: '100%' }}
+        >
+          <Form
+            name="login"
+            layout="vertical"
+            labelAlign="left"
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              required
+              rules={[
+                { required: true, message: "Please input email"}, 
+                { type: 'email', message: 'Invalid email'}
+              ]}
+            >
+              <Input/>
+            </Form.Item>
+            <Form.Item>
+              <Button  
+                type="primary" 
+                style={{ width: '100%', marginTop: 15, marginBottom: 15 }}
+              >
+                Send Email Instructions
+              </Button>
+
+              <Link to={'/login'}>
+                <Button 
+                  type="text"
+                  style={{ width: '100%' }}
+                >
+                  Back to Login
+                </Button>
+              </Link>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
   )
 }
